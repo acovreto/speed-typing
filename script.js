6d939311e85ee21ee8996fa0d9e4164f584bab4b
Aco Vreto <acovretoski@gmail.com>
2023-11-17 13:02:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// List of words for game
+const words = [
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "quince",
+  "eight",
+  "feeble",
+  "admit",
+  "drag",
+  "loving",
+];
